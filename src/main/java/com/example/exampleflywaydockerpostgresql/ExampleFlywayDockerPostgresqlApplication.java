9df3bb016408d020dfe2dc3ba3eb1e43689658a7
flyin <flying.falcon@outlook.com>
2023-11-16 19:10:09 +0100

    initial commit

---
+++ src/main/java/com/example/exampleflywaydockerpostgresql/ExampleFlywayDockerPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleflywaydockerpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleFlywayDockerPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleFlywayDockerPostgresqlApplication.class, args);
+	}
+
+}
